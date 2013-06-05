c434d921496149683a6ca007acaf641b1fb7d5a9
Adrian Toman <adrian.toman@gooddata.com>
2013-06-05 11:24:35 +0200

    Debuging

--- lib/helpers/helper.rb
+++ lib/helpers/helper.rb
@@ -65,7 +65,7 @@ module SLAWatcher
     end
 
     def self.interval_to_minutes(interval)
-      pp interval
+      @@log.info interval
       values = interval.split(":")
       hours = Integer(values[0])
       minutes = Integer(values[1])
