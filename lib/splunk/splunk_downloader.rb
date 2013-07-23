0e9be74aa6baec9ac05d597768c58ab659c6e612
Adrian Toman <adrian.toman@gooddata.com>
2013-07-23 14:59:49 +0200

    Finish test change + google spredsheet synchronization

--- lib/splunk/splunk_downloader.rb
+++ lib/splunk/splunk_downloader.rb
@@ -9,7 +9,7 @@ module SLAWatcher
     def initialize(username, password,hostname)
       @splunk = SplunkClient.new(username ,password, hostname)
 
-      @last_runs_query = 'eventtype=MSF mode (component="workers.clover-executor" OR component="workers.clover-status") starttime=%START_TIME% endtime=%END_TIME%  action=worker_run (status=STARTED OR status=FINISHED OR status=ERROR) ( %PIDS% )
+      @last_runs_query = 'eventtype=MSF mode (component="workers.clover-executor" OR component="workers.clover-status") starttime=%START_TIME% endtime=%END_TIME%  action=process_run (status=STARTED OR status=FINISHED OR status=ERROR) ( %PIDS% )
                         | fields project_id, request_id,transformation_id, clover_graph, mode, status, _time
                         | table project_id, request_id,transformation_id, clover_graph, mode, status, _time'
 
