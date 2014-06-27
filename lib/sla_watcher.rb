a475af7c8ad51a835ac95df33b5e4c97c0aecf84
Adrian Toman <adrian.toman@gooddata.com>
2014-06-27 13:28:40 +0200

    Remove removal_task from list

--- lib/sla_watcher.rb
+++ lib/sla_watcher.rb
@@ -17,7 +17,7 @@ require "passwordmanager"
 %w(synchronize execution).each {|a| require_relative "snifer/#{a}"}
 %w(google_downloader).each {|a| require_relative "google/#{a}"}
 %w(testcases).each {|a| require_relative "testcases/#{a}"}
-%w(notification_removal_task).each {|a| require_relative "custom/#{a}"}
+#%w(notification_removal_task).each {|a| require_relative "custom/#{a}"}
 
 
 module SLAWatcher
