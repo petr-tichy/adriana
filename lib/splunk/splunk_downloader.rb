48bb5fe5b1c698d9847c0fe75e07e8c511201a45
Adrian Toman <adrian.toman@gooddata.com>
2014-08-29 09:00:50 +0200

    Spliting splunk query to multiple queries

--- lib/splunk/splunk_downloader.rb
+++ lib/splunk/splunk_downloader.rb
@@ -15,7 +15,7 @@ module SLAWatcher
                         | fields project_id, request_id,transformation_id, mode, status, _time | rex field=_raw "clover_graph=(?<clover_graph>[^=]+) [^=]+="
                         | table project_id, request_id,transformation_id, clover_graph, mode, status, _time'
 
-
+      @ONE_QUERY_LIMIT = 1000
       #@start_query = 'eventtype=MSF mode component="workers.clover-executor" starttime=%START_TIME% endtime=%END_TIME%  action=worker_run status=STARTED ( %PIDS% ) | fields project_id, request_id,transformation_id, clover_graph, mode, status, _time | table project_id, request_id,transformation_id, clover_graph, mode, status, _time'
       #@finish_error_query = ''
 
@@ -35,19 +35,26 @@ module SLAWatcher
 
     def load_runs(from,to,projects)
       project_strings = []
+      temp_project_string = []
       projects.each do |p|
-         project_strings.push("project_id=#{p.project_pid}")
+        temp_project_string.push("project_id=#{p.project_pid}")
+        if (temp_project_string.count == @ONE_QUERY_LIMIT)
+          project_strings << temp_project_string
+          temp_project_string = []
+        end
       end
-
-      query = @last_runs_query.sub("%PIDS%",project_strings.join(" OR "));
-      query = query.sub("%START_TIME%",from.strftime("%m/%d/%Y:%H:%M:%S"));
-      query = query.sub("%END_TIME%",to.strftime("%m/%d/%Y:%H:%M:%S"));
-      result = execute_query(query)
+      project_strings << temp_project_string
 
       values = []
+      project_strings.each do |temp|
+        query = @last_runs_query.sub("%PIDS%",temp.join(" OR "));
+        query = query.sub("%START_TIME%",from.strftime("%m/%d/%Y:%H:%M:%S"));
+        query = query.sub("%END_TIME%",to.strftime("%m/%d/%Y:%H:%M:%S"));
+        result = execute_query(query)
 
-      result.parsedResults.each do |p|
-        values.push({:project_pid => p.project_id,:request_id => p.request_id, :clover_graph => Helper.extract_graph_name(p.clover_graph), :mode => Helper.extract_mode(p.mode), :status => p.status,:time => DateTime.strptime(p._time,"%Y-%m-%dT%H:%M:%S.%L%z")})
+        result.parsedResults.each do |p|
+          values.push({:project_pid => p.project_id,:request_id => p.request_id, :clover_graph => Helper.extract_graph_name(p.clover_graph), :mode => Helper.extract_mode(p.mode), :status => p.status,:time => DateTime.strptime(p._time,"%Y-%m-%dT%H:%M:%S.%L%z")})
+        end
       end
       values.sort{|a,b| a[:time] <=> b[:time]}
     end
