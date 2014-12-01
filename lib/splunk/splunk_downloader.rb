ae429433137ef317aaf0b7e2ad54052acd8cd6b6
MS@clover-sla <MS@clover-sla>
2014-12-01 12:05:40 +0100

    Add "source" to splunk query

--- lib/splunk/splunk_downloader.rb
+++ lib/splunk/splunk_downloader.rb
@@ -12,11 +12,11 @@ module SLAWatcher
       password = PasswordManagerApi::Password.get_password_by_name(username.split("|").first,username.split("|").last)
       @splunk = SplunkClient.new(username.split("|").last,password, hostname)
 
-      @last_runs_query = 'eventtype=MSF earliest=%START_TIME% latest=%END_TIME%  action=process_run (component="workers.clover-executor" OR component="workers.clover-status") (status=STARTED OR status=FINISHED OR status=ERROR) ( %PIDS% )
+      @last_runs_query = 'source="/mnt/log/gdc-java" sourcetype=log4j eventtype=MSF earliest=%START_TIME% latest=%END_TIME%  action=process_run (component="workers.clover-executor" OR component="workers.clover-status") (status=STARTED OR status=FINISHED OR status=ERROR) ( %PIDS% )
                         | fields project_id, request_id,transformation_id, mode, status, _time | rex field=_raw "clover_graph=(?<clover_graph>[^=]+) [^=]+="
                         | table project_id, request_id,transformation_id, clover_graph, mode, status, _time'
  
-      @ONE_QUERY_LIMIT = 1000
+      @ONE_QUERY_LIMIT = 300
       #@start_query = 'eventtype=MSF mode component="workers.clover-executor" starttime=%START_TIME% endtime=%END_TIME%  action=worker_run status=STARTED ( %PIDS% ) | fields project_id, request_id,transformation_id, clover_graph, mode, status, _time | table project_id, request_id,transformation_id, clover_graph, mode, status, _time'
       #@finish_error_query = ''
 
