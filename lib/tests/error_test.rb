74567c350b7290aacd493a5a76701bd8b6b808d2
Kryštof Šplíchal <krystof.splichal@gooddata.com>
2018-05-17 16:00:29 +0200

    ErrorTest refactor

--- lib/tests/error_test.rb
+++ lib/tests/error_test.rb
@@ -1,20 +1,16 @@
 module SLAWatcher
-
   # This test will be executed every 15mins and will test, if all of the contracts, which have monitoring enabled, are going well
   # If not, for each contract only one event will be created and this event will be resend (every 1 hour) until resolved
-
-
   class ErrorTest < BaseTest
-
+    EVENT_TYPE = 'ERROR_TEST'.freeze
+    SEVERITY = Severity.HIGH
 
     def initialize
       super()
-      @EVENT_TYPE = 'ERROR_TEST'
-      @SEVERITY = Severity.HIGH
     end
 
     def start
-      @@log.info "Starting the #{@EVENT_TYPE} test"
+      @@log.info "Starting the #{EVENT_TYPE} test"
       @new_events = []
       load_data
       @schedules.each do |s|
@@ -22,47 +18,41 @@ module SLAWatcher
         next if executions.nil?
         last_execution = executions.last
         # Lets look on last execution and see if there is some problem on the way
-        if last_execution.status == 'ERROR' or (last_execution.status == 'RUNNING' and !executions[-2].nil? and executions[-2].status == 'ERROR')
-          # Last execution is Running or ERROR ... we need to go deeper in history to check if there is some reccuring problem
-          last_ok_status_index = -1
-          executions.each_with_index do |execution, i|
-            last_ok_status_index = i if execution.status == 'FINISHED'
-          end
-          # Lets count the number of consequent errors
-          if executions.count - last_ok_status_index - 1 > s.max_number_of_errors
-            # eah we need to create PD error
-            executions.each_with_index do |e, index|
-              if index > last_ok_status_index and e.status == 'ERROR'
-                unless @notification_logs.has_key? e.id.to_s
-                  base_text = "The execution for this schedule has failed. This is #{index - last_ok_status_index} error in row."
-                  error_text = e.error_text
-                  @new_events << CustomEvent.new(
-                      Key.new(e.id, @EVENT_TYPE),
-                      @SEVERITY,
-                      base_text,
-                      @now,
-                      nil,
-                      s.id,
-                      nil,
-                      error_text
-                  )
-                end
-              end
-            end
-          end
+        next unless last_execution.status == 'ERROR' || (last_execution.status == 'RUNNING' && !executions[-2].nil? && executions[-2].status == 'ERROR')
+        # Last execution is Running or ERROR ... we need to go deeper in history to check if there is some reccuring problem
+        last_ok_status_index = -1
+        executions.each_with_index do |execution, i|
+          last_ok_status_index = i if execution.status == 'FINISHED'
+        end
+        # Count the number of consequent errors
+        next unless executions.count - last_ok_status_index - 1 > s.max_number_of_errors
+        # Create a PD error
+        executions.each_with_index do |e, index|
+          next unless index > last_ok_status_index && e.status == 'ERROR' && !@notification_logs.has_key?(e.id.to_s)
+          error_order = index - last_ok_status_index
+          base_text = "The execution for this schedule has failed. This is #{error_order} error in row."
+          error_text = e.error_text
+          @new_events << CustomEvent.new(
+            Key.new(e.id, EVENT_TYPE),
+            SEVERITY,
+            base_text,
+            @now,
+            nil,
+            s.id,
+            nil,
+            error_text
+          )
         end
       end
-      @@log.info "The test #{@EVENT_TYPE} has finished. Created #{@new_events.count} events"
+      @@log.info "The test #{EVENT_TYPE} has finished. Created #{@new_events.count} events"
       @new_events
     end
 
     def load_data
       @now = DateTime.now
       @schedules = Schedule.joins(:contract).where(project: {is_deleted: false, status: 'Live'}, contract: {is_deleted: false}, schedule: {is_deleted: false})
-      @notification_logs = NotificationLog.where(notification_type: @EVENT_TYPE, created_at: (@now - 9.day)..@now).group_by { |x| x.key }
+      @notification_logs = NotificationLog.where(notification_type: EVENT_TYPE, created_at: (@now - 9.day)..@now).group_by { |x| x.key }
       @last_five_executions = ExecutionLog.get_last_five_executions_per_schedule.group_by { |x| x.r_schedule }
     end
-
   end
-
 end
