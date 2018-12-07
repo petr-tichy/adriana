0e8fdb7411cc580c8b6880e84361c8dfa4b8d7d6
Kryštof Šplíchal <krystof.splichal@gooddata.com>
2018-12-07 12:37:58 +0100

    Add Salesforce monitoring synchronization

--- lib/sla_jobs/salesforce_synchronization_job/salesforce_synchronization_job.rb
+++ lib/sla_jobs/salesforce_synchronization_job/salesforce_synchronization_job.rb
@@ -1,11 +1,13 @@
 require 'restforce'
 require_relative '../job_exception'
 require_relative '../job_helper'
-require 'pry'
 
 module SalesforceSynchronizationJob
   class SalesforceSynchronizationJob
     JOB_TYPE = 'salesforce_synchronization'.freeze
+    SF_MONITORING_FIELD = 'Managed__c'.freeze
+    SF_TOKEN_FIELD = 'Platform_Token__c'.freeze
+
     attr_accessor :credentials, :sf_client
 
     def initialize(credentials)
@@ -13,27 +15,60 @@ module SalesforceSynchronizationJob
     end
 
     def connect
-      #self.class.connect_to_sf
+      connect_to_sf
     end
 
     def run
-      client = Restforce.new(username: 'integration.internal@gooddata.com',
-                             password: '',
-                             security_token: '',
-                             client_id: '',
-                             client_secret: '',
-                             api_version: '41.0')
-      binding.pry
-      output = client.query("SELECT Id, Managed__c, Platform_Token__c FROM Contract")
-      client.update('Contract', Id: contract_id, Managed__c: true)
+      @sf_contracts = sf_contract_mapping
+      $log.info "Processing #{live_contracts.size} monitored contracts."
+      live_contracts.each do |contract|
+        process_contract_monitoring(contract, monitoring_value: true)
+      end
+      $log.info "Processing #{deleted_contracts.size} non-monitored contracts."
+      deleted_contracts.each do |contract|
+        process_contract_monitoring(contract, monitoring_value: false)
+      end
     end
 
-    class << self
-      def connect_to_sf
-        connection_params = %i[client_id client_secret username password security_token]
-        fail "All of the following parameters must be provided for the Salesforce client: #{connection_params}" unless connection_params.all? { |x| !@credentials[:salesforce][x].to_s.empty? }
-        @sf_client = Restforce.new(connection_params.map { |x| [x, @credentials[:salesforce][x]] }.to_h.merge(api_version: '41.0'))
+    def process_contract_monitoring(contract, monitoring_value: true)
+      reference_name = "#{contract.id} - #{contract.name}"
+      local_token = contract.token
+      if local_token.to_s.empty?
+        $log.warn "Contract #{reference_name} has no 'token' value and cannot be synchronized."
+        return false
       end
+      unless @sf_contracts.key?(local_token)
+        $log.warn "Cannot find SF contract with platform token #{local_token}."
+        return false
+      end
+      sf_object = @sf_contracts[local_token]
+      return if sf_object[:sf_monitoring_field] == monitoring_value
+      $log.info "Updating Contract #{sf_object[:id]} - setting #{SF_MONITORING_FIELD} to #{monitoring_value}."
+      @sf_client.update('Contract', :Id => sf_object[:id], SF_MONITORING_FIELD.to_sym => monitoring_value)
+      true
+    end
+
+    # Contracts are mapped by platform token field
+    def sf_contract_mapping
+      output = @sf_client.query("SELECT Id, #{SF_MONITORING_FIELD}, #{SF_TOKEN_FIELD} FROM Contract")
+      tokens = output.map { |x| [x[SF_TOKEN_FIELD], x['Id']].flatten }
+      non_unique = tokens.select { |x| tokens.count { |y| y[0] == x[0] } > 1 }
+      fail "There are multiple contracts for a single token - #{non_unique.map { |x| x.join '-' }.join(', ')}. Aborting." if non_unique.any?
+      output.map { |x| [x[SF_TOKEN_FIELD], {id: x['Id'], sf_monitoring_field: x[SF_MONITORING_FIELD]}] }.to_h
+    end
+
+    def live_contracts
+      Contract.all.where(is_deleted: false)
+    end
+
+    def deleted_contracts
+      Contract.all.where(is_deleted: true)
+    end
+
+    def connect_to_sf
+      connection_params = %i[client_id client_secret username password security_token]
+      fail "All of the following parameters must be provided for the Salesforce client: #{connection_params}" if connection_params.any? { |x| @credentials.dig(:salesforce, x).to_s.empty? }
+      @sf_client = Restforce.new(connection_params.map { |x| [x, @credentials[:salesforce][x]] }.to_h.merge(api_version: '41.0'))
     end
   end
 end
--- lib/tasks/synchronize_salesforce.rake
+++ lib/tasks/synchronize_salesforce.rake
@@ -2,15 +2,13 @@ require_relative '../sla_jobs/salesforce_synchronization_job/salesforce_synchron
 require_relative '../credentials_helper'
 
 namespace :sla do
-  desc 'Synchronize with Salesforce' #TODo
+  desc 'Synchronize with Salesforce - set monitoring field for all Contracts'
   task synchronize_salesforce: [:environment] do |task|
     task_name = task.name.split(':').last
-    #$log_file = Rails.root.join('log', "sla_#{task_name}_#{DateTime.now.strftime('%Y%m%d%H%M%S')}.log")
-    #$log = Logger.new($log_file, 'daily')
-    $log = Logger.new(STDOUT)
-    credentials = {
-      #TODO
-    }
+    $log_file = Rails.root.join('log', "sla_#{task_name}_#{DateTime.now.strftime('%Y%m%d%H%M%S')}.log")
+    $log = Logger.new($log_file, 'daily')
+    credentials = {salesforce: {}}
+    %i[client_id client_secret username password security_token].each { |param| credentials[:salesforce][param] = CredentialsHelper.get("salesforce_#{param}") }
     job = SalesforceSynchronizationJob::SalesforceSynchronizationJob.new(credentials)
     begin
       job.connect
